49bead049326470e9afd9f1a6160ad49cbd45e7c
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-02-01 02:08:56 -0300

    Projeto finalizado

--- Projeto-cadastro-simples/backend/app.js
+++ Projeto-cadastro-simples/backend/app.js
@@ -10,7 +10,21 @@ mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
 // Middlewares
-server.get('/', (req. res, next) => res.send('BackEnd'))
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+server.use(cors())
+
+// ODM
+const Client = restful.model('Client', {
+    name: { type: String, required: true }
+})
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true})
+
+// Routes
+Client.register(server, '/clients')
 
 // Start Server
 server.listen(3000)
